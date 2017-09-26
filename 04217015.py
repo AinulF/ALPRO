2db2ff0cd260ca0d9a4cfc3a1896550d6bfcd9df
AinulF <31976669+AinulF@users.noreply.github.com>
2017-09-26 22:44:47 +0700

    04217015.py

---
+++ 04217015.py
@@ -0,0 +1,8 @@
+# x = raw_input("Masukkan Nama Anda	:	")
+y = input("Masukkan NIM Anda	:	")
+# z = input("Masukkan Umur Anda	:	")
+
+# print"Nama : " ,x
+print"NIM : " ,y
+# print"umur ; " ,z
+# print"umur anda 5 tahun kedepan adalah " ,z+5
